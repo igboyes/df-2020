61175f2a89177b818774b5b8fe05ec583f1dd23b
Ian Boyes <igboyes@gmail.com>
2020-01-30 10:58:27 -0800

    Add arg parsing to filter_db.py

--- filter_df.py
+++ filter_df.py
@@ -1,10 +1,25 @@
+from argparse import ArgumentParser
 from Bio import SeqIO
 
-with open("data/sorted_unique.txt", "r") as f:
+parser = ArgumentParser()
+
+parser.add_argument(
+    "headers",
+    help="path to text file containing list of headers"
+)
+
+parser.add_argument(
+    "fasta",
+    help="path to FASTA file to filter by header list"
+)
+
+args = parser.parse_args()
+
+with open(args.headerse, "r") as f:
     seq_ids = {line.rstrip() for line in f}
 
 
-with open("data/df_genome.fna", "r") as f:
+with open(args.fasta, "r") as f:
     for record in SeqIO.parse(f, "fasta"):
         if record.name in seq_ids:
             print(f">{record.name}")
